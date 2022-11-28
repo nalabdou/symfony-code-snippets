69be12e8fa257741f4b92896468b47d8f36baa61
nadim alabdou <nadim.alabdou@gmail.com>
2022-11-28 20:58:47 +0100

    add doctrine, httpfoundation,security and contributes folder

---
+++ src/constant/Meta.ts
@@ -0,0 +1,8 @@
+const Meta = {
+    creator: 'AL ABDOU Nadim',
+    welcome: 'Thank you for installing this extension 💜',
+    github: 'https://github.com/nalabdou',
+    followMe: 'Follow me on Github',
+    dissmiss: 'Dissmiss',
+}
+export default Meta;
