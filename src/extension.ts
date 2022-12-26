392c0bf40520d480b3c010f9cb8cc4bf3bcd7f44
nadim alabdou <nadim.alabdou@gmail.com>
2022-12-26 16:54:57 +0100

    Remove welcome message

--- src/extension.ts
+++ src/extension.ts
@@ -4,7 +4,7 @@ import { open } from './helper/http';
 import { info } from './Notifcation/Notification';
 
 export function activate(context: vscode.ExtensionContext) {
-    const notifcation = info(Meta.welcome, Meta.followMe, Meta.dissmiss);
-    notifcation.then((value) => value === Meta.followMe && open(Meta.github));
+    // const notifcation = info(Meta.welcome, Meta.followMe, Meta.dissmiss);
+    // notifcation.then((value) => value === Meta.followMe && open(Meta.github));
 }
 
